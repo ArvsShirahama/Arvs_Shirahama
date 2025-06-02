ecf1f35a6a463477a2b663497a31eb89863544f9
ArvsShirahama <arvsshirahama8@gmail.com>
2025-06-02 20:48:14 +0800

    Delete Starter Pack - Copy directory

--- Starter Pack - Copy/script.js
+++
@@ -1,4 +0,0 @@
-document.querySelector('form').addEventListener('submit', function (e) {
-    e.preventDefault(); // Prevent form from refreshing the page
-    alert('Thank you for your message! This is a demo form.');
-});
